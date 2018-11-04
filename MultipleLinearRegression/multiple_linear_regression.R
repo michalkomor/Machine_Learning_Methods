8d2699c5648d9d07d042b3e8b654ad11bccf0b68
Michał Komor <43879974+michalkomor@users.noreply.github.com>
2018-11-04 21:31:54 +0100

    v5

--- MultipleLinearRegression/multiple_linear_regression.R
+++ MultipleLinearRegression/multiple_linear_regression.R
@@ -29,3 +29,32 @@ y_pred = predict(regressor, newdata = test_set)
 y_pred
 
 #Building the optimal model using Backward Elimination
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
+               data = training_set)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend + Marketing.Spend,
+               data = training_set)
+summary(regressor)
+regressor = lm(formula = Profit ~ R.D.Spend,
+               data = training_set)
+summary(regressor)
+
+#Automatic implementation of Backward Elimination
+backwardElimination <- function(x, sl) {
+  numVars = length(x)
+  for (i in c(1:numVars)){
+    regressor = lm(formula = Profit ~ ., data = x)
+    maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+    if (maxVar > sl){
+      j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+      x = x[, -j]
+    }
+    numVars = numVars - 1
+  }
+  return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
